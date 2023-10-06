351988d4e767cc242fe66cb552277b404c1aeac7
willi <williangel10@outlook.com>
2023-10-06 14:00:41 -0500

    "v1.1.1"

--- Warshall.cpp
+++ Warshall.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "Warshall.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace  std;
+
+
+void Warshall::leerMatriz(std::string fileName) {
+    ifstream archivo("arista.txt");
+
+    if(!archivo.is_open()){
+        cout<<"No se pudo abrir el archivo"<<endl;
+    }
+}
